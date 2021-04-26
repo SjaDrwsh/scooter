656e79f1c382c00396e4308bd1a3fbbcf86a2ab3
sdr <sdr@knowis.de>
2021-04-27 00:13:03 +0200

    solving wunder mobility assignment

---
+++ config/jest/testEnvironment.js
@@ -0,0 +1,28 @@
+/** Create a dedicated module that will be used for setting up the jsdom test environment. */
+// TODO:use this when we can delete again jest-environment-jsdom-sixteen
+//const JSDOMEnvironment = require('jest-environment-jsdom');
+const JSDOMEnvironment = require('jest-environment-jsdom-sixteen');
+
+/**
+ *  The module must export a class with setup, teardown and runScript methods.
+ *  See more documentation here: https://jestjs.io/docs/en/configuration.html#testenvironment-string
+ **/
+module.exports = class JSDOMEnvironmentGlobal extends JSDOMEnvironment {
+  constructor(config, context) {
+    super(config, context);
+  }
+
+  async setup() {
+    await super.setup();
+    this.global.jsdom = this.dom;
+  }
+
+  async teardown() {
+    this.global.jsdom = null;
+    await super.teardown();
+  }
+
+  runScript(script) {
+    return super.runScript(script);
+  }
+};
